d43a71fa60afa50b2ac33bf48e7f14eb209143b1
malchul <None>
2020-10-19 10:33:28 +0300

    0.898

--- src/model.py
+++ src/model.py
@@ -16,6 +16,7 @@ class NLPClassifierModel(nn.Module):
         self,
         num_classes,
         input_channels,
+        feat_size=256,
         hidden_size=128, # Best on MLP
         num_layers=2,
         clip=1e-4
@@ -24,8 +25,14 @@ class NLPClassifierModel(nn.Module):
 
         self.clip = clip
 
-        self.body_net = nn.LSTM(input_size=input_channels, hidden_size=hidden_size, num_layers=num_layers, batch_first=False, bidirectional=True)
-        self.title_net = nn.LSTM(input_size=input_channels, hidden_size=hidden_size, num_layers=num_layers, batch_first=False, bidirectional=True)
+        self.body_inner_map = nn.Sequential(nn.Conv1d(input_channels, feat_size, 1),
+                                            nn.GroupNorm(NUM_GROUPS, feat_size), nn.ReLU(inplace=True))
+        self.body_net = nn.LSTM(input_size=feat_size, hidden_size=hidden_size, num_layers=num_layers,
+                                batch_first=False, bidirectional=True)
+
+        self.title_inner_map = nn.Sequential(nn.Conv1d(input_channels, feat_size, 1),
+                                             nn.GroupNorm(NUM_GROUPS, feat_size), nn.ReLU(inplace=True))
+        self.title_net = nn.LSTM(input_size=feat_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=False, bidirectional=True)
         self.tags_net = nn.Linear(input_channels, hidden_size)
         self.hidden = nn.Sequential(nn.Linear(hidden_size * 3, hidden_size), nn.GroupNorm(NUM_GROUPS, hidden_size),
                                     nn.ReLU(inplace=True))
@@ -39,18 +46,19 @@ class NLPClassifierModel(nn.Module):
         self.act = nn.Softmax(dim=1)
 
 
-
-    def pack_items(self, items):
+    def pack_items(self, items, map_nn=None):
         lengths = [item.shape[0] for item in items]
         padded_items = pad_sequence(items)
+        if map_nn is not None:
+            padded_items = map_nn(padded_items.permute(1,2,0)).permute(2,0,1)
         items = pack_padded_sequence(padded_items, lengths, enforce_sorted=False)
         return items
 
     def forward(self, body, title, tags):
         #print(type(body), type(title), type(tags))
         tags_output = self.tags_net(tags)
-        _, (title_output, _) = self.title_net(self.pack_items(title))
-        _, (body_output,_) = self.body_net(self.pack_items(body))
+        _, (title_output, _) = self.title_net(self.pack_items(title, self.title_inner_map))
+        _, (body_output,_) = self.body_net(self.pack_items(body, self.body_inner_map))
 
 
         #print(body_output.shape, title_output.shape, tags_output.shape)
